a8f14a9c5b5fd528ba71209bca5154098d62acd3
dhinakaran s <53988061+dhinakaran011@users.noreply.github.com>
2023-01-26 21:07:49 +0530

    School.java

---
+++ School.java
@@ -0,0 +1,29 @@
+package Assignments.project;
+
+class School{
+
+int mark;
+
+private int salary;
+
+static String school_name = "St. Antony's Primary School";
+
+    
+
+public static void main(String args[]){
+
+}
+
+void conduct_exams(){
+
+System.out.println();
+
+}
+
+void publish_results(int mark){
+
+System.out.println(mark);
+
+}
+
+}
