07b99af9d6f1b1d2c86736f1b5c08c5ab5a2f7ee
dhinakaran s <53988061+dhinakaran011@users.noreply.github.com>
2023-01-26 21:09:27 +0530

    Teacher.java

---
+++ Teacher.java
@@ -0,0 +1,19 @@
+package Assignments.project;
+
+class Teacher{
+
+public static void main(String args[]){
+
+School teacher = new School();
+
+teacher.conduct_exams();
+
+teacher.publish_results(75);
+
+System.out.println(teacher.school_name);
+
+System.out.println(teacher.salary);
+
+}
+
+}
